22b4d642ec231f3bbc5200953399763092b1960a
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-07-18 10:53:45 +0200

    added new exercises

---
+++ Lezione6,5-1707/jsQuiz.js
@@ -0,0 +1,119 @@
+"use strict";
+const domande = [
+    "Qual è la capitale della Danimarca?",
+    "Qual è la capitale della Svezia?",
+    "Qual è la capitale della Norvegia?",
+    "Qual è la capitale della Finlandia?",
+    "Qual è la capitale dell'Islanda?",
+    "Qual è la capitale della Groenlandia?",
+    "Qual è la capitale Dell'Italia?",
+    "Qual è la capitale della Francia?",
+    "Qual è la capitale della Spagna?",
+    "Qual è la capitale del Portogallo?",
+    "Qual è la capitale della Germania?",
+    "Qual è la capitale dell'Olanda?",
+    "Qual è la capitale del Belgio?",
+    "Qual è la capitale della Svizzera?",
+    "Qual è la capitale dell'Austria?",
+    "Qual è la capitale della Polonia?",
+    "Qual è la capitale della Repubblica Ceca?",
+    "Qual è la capitale della Slovacchia?",
+    "Qual è la capitale dell'Ungheria?",
+    "Qual è la capitale della Romania?",
+    "Qual è la capitale della Bulgaria?",
+    "Qual è la capitale della Grecia?",
+    "Qual è la capitale della Turchia?",
+    "Qual è la capitale della Russia?",
+    "Qual è la capitale dell'Ucraina?",
+    "Qual è la capitale della Bielorussia?",
+    "Qual è la capitale della Lituania?",
+    "Qual è la capitale della Lettonia?",
+    "Qual è la capitale dell'Estonia?",
+    "Qual è la capitale della Moldavia?",
+    "Qual è la capitale della Croazia?",
+    "Qual è la capitale della Slovenia?",
+    "Qual è la capitale della Bosnia-Erzegovina?",
+    "Qual è la capitale del Montenegro?",
+    "Qual è la capitale dell'Albania?",
+    "Qual è la capitale della Macedonia del Nord?",
+    "Qual è la capitale della Serbia"
+];
+const risposte = [
+    ["V|Copenaghen", "F|Oslo", "F|Stoccolma"],
+    ["F|Copenaghen", "F|Oslo", "V|Stoccolma"],
+    ["F|Copenaghen", "V|Oslo", "F|Stoccolma"],
+    ["F|Copenaghen", "F|Oslo", "V|Helsinki"],
+    ["V|Reykjavik", "F|Oslo", "F|Stoccolma"],
+    ["F|Copenaghen", "F|Oslo", "V|Nuuk"],
+    ["F|Copenaghen", "F|Oslo", "V|Roma"],
+    ["F|Copenaghen", "F|Oslo", "V|Parigi"],
+    ["F|Copenaghen", "F|Oslo", "V|Madrid"],
+    ["F|Copenaghen", "F|Oslo", "V|Lisbona"],
+    ["F|Copenaghen", "F|Oslo", "V|Berlino"],
+    ["F|Copenaghen", "V|Amsterdam", "F|Stoccolma"],
+    ["F|Copenaghen", "V|Bruxelles", "F|Stoccolma"],
+    ["F|Copenaghen", "F|Oslo", "V|Berna"],
+    ["F|Copenaghen", "F|Oslo", "V|Vienna"],
+    ["F|Copenaghen", "F|Oslo", "V|Varsavia"],
+    ["F|Copenaghen", "F|Oslo", "V|Praga"],
+    ["F|Copenaghen", "F|Oslo", "V|Bratislava"],
+    ["F|Copenaghen", "F|Oslo", "V|Budapest"],
+    ["F|Copenaghen", "F|Oslo", "V|Bucarest"],
+    ["F|Copenaghen", "F|Oslo", "V|Sofia"],
+    ["F|Copenaghen", "F|Oslo", "V|Atene"],
+    ["F|Copenaghen", "F|Oslo", "V|Ankara"],
+    ["F|Copenaghen", "F|Oslo", "V|Mosca"],
+    ["F|Copenaghen", "F|Oslo", "V|Kiev"],
+    ["F|Copenaghen", "F|Oslo", "V|Minsk"],
+    ["F|Copenaghen", "F|Oslo", "V|Vilnius"],
+    ["F|Copenaghen", "F|Oslo", "V|Riga"],
+    ["F|Copenaghen", "F|Oslo", "V|Tallinn"],
+    ["F|Copenaghen", "F|Oslo", "V|Chisinau"],
+    ["F|Copenaghen", "F|Oslo", "V|Zagabrìa"],
+    ["F|Copenaghen", "F|Oslo", "V|Lubiana"],
+    ["F|Copenaghen", "F|Oslo", "V|Sarajevo"],
+    ["F|Copenaghen", "F|Oslo", "V|Podgorica"],
+    ["F|Copenaghen", "F|Oslo", "V|Tirana"],
+    ["F|Copenaghen", "F|Oslo", "V|Skopje"],
+    ["F|Copenaghen", "F|Oslo", "V|Belgrado"]
+];
+$(document).ready(function () {
+    creaHTML();
+});
+
+function creaHTML() {
+    let strHTML = "<table>";
+    for (let i = 0; i < domande.length; i++) {
+        strHTML += "<tr>";
+        strHTML += "<td>" + domande[i] + "</td>";
+        strHTML += "<td>";
+        strHTML += "<select id='risposta" + i + "'>";
+        for (let j = 0; j < risposte[i].length; j++) {
+            let elemento = risposte[i][j];
+            let valElement = elemento.split('|')[0];
+            let valText = elemento.split('|')[1];
+            strHTML += "<option value='" + valElement + "'>" + valText + "</option>";
+        }
+        strHTML += "</select>";
+        strHTML += "</td>";
+        strHTML += "</tr>";
+    }
+    strHTML += "<tr><td colspan='2'><button id='btnCalcola' onclick='quiz();'>CALCOLA</button></td></tr>";
+    strHTML += "</table>";
+    if (document.getElementById("quiz") != null) {
+        // document.getElementById("quiz").innerHTML = strHTML;
+        $("#quiz").html(strHTML);
+    }
+}
+
+function quiz() {
+    let risposteEsatte = 0;
+    for (let i = 0; i < domande.length; i++) {
+        // if (document.getElementById("risposta" + i).value === "V")
+        if ($("#risposta" + i).val() === "V") {
+            risposteEsatte++;
+        }
+    }
+    // document.getElementById("risultato").innerHTML = "Risposte esatte: " + risposteEsatte + " su " + domande.length;
+    $("#risultato").html("Risposte esatte: " + risposteEsatte + " su " + domande.length);
+}
---
+++ Lezione7-1807/Classi.js
@@ -0,0 +1,9 @@
+"use strict";
+class Animale {
+    constructor(nome, razza) {
+        this.nome = nome;
+        this.razza = razza;
+    }
+}
+let cane = new Animale("Fido", "Pastore Tedesco");
+console.log(cane);
